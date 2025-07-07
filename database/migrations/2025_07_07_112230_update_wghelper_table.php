bef50b2687ac34bb587305f7c2026b062c024463
andreasalbers <mail@andreasalbers.de>
2025-07-07 11:27:05 +0200

    2025-07-07 Version 3

---
+++ database/migrations/2025_07_07_112230_update_wghelper_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('wghelper', function (Blueprint $table) {
+            if (!Schema::hasColumn('wghelper', 'name')) {
+                $table->string('name', 80)
+                    ->nullable()
+                    ->comment('')
+                    ->after('wgnr');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('wghelper', function (Blueprint $table) {
+            if (Schema::hasColumn('wghelper', 'name')) {
+                $table->dropColumn('name');
+            }
+        });
+    }
+};
